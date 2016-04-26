67f44068abc5e2f70189845132a7e0ccaecd83ea
jaredostdiek <jaredostdiek@gmail.com>
2016-04-26 01:42:03 -0700

    Created a Firebase backend. Added ability to register users with email through Firebase.

--- app/src/main/java/com/me202/jaredostdiek/smartbikepart1/LoginActivity.java
+++ app/src/main/java/com/me202/jaredostdiek/smartbikepart1/LoginActivity.java
@@ -6,10 +6,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import java.util.HashMap;
 import java.util.Map;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
 
 /**
  * Created by jaredostdiek on 4/4/16.
@@ -20,18 +27,22 @@ public class LoginActivity extends AppCompatActivity {
 
     Button loginButton, nextButton, registerButton;
     EditText userInput, passInput;
-    Map<String,String > users = new HashMap(); //stores usernames and passwords
+    //Map<String,String > users = new HashMap(); //stores usernames and passwords
     Context context = this;
     String usernameStr;
     Bundle loginBundle;
+    Firebase myFirebaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //create firebase reference
+        myFirebaseRef = new Firebase("https://vivid-heat-8090.firebaseio.com/");
+
         //add user and password to map
-        users.put(context.getString(R.string.usersKeyJared), context.getString(R.string.usersValJared));
+        //users.put(context.getString(R.string.usersKeyJared), context.getString(R.string.usersValJared));
 
         //create objects for button and user inputs
         loginButton = (Button)findViewById(R.id.loginButton);
@@ -44,78 +55,148 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 usernameStr = userInput.getText().toString();
-                if (users.containsKey(usernameStr)) {
-                    passInput.setVisibility(View.VISIBLE);
-                    passInput.setHint(R.string.passwordHint);
-                    loginButton.setVisibility(View.VISIBLE);
-                    nextButton.setVisibility(View.GONE);
-                }
-                else{
-                    Toast.makeText(getApplicationContext(), R.string.registerToast, Toast.LENGTH_SHORT).show();
-                    passInput.setVisibility(View.VISIBLE);
-                    passInput.setHint(R.string.setPasswordHint);
-                    registerButton.setVisibility(View.VISIBLE);
-                    nextButton.setVisibility(View.GONE);
 
+                //test if user email exists
+                myFirebaseRef.createUser(usernameStr, "test", new Firebase.ValueResultHandler<Map<String, Object>>() {
+                    @Override
+                    public void onSuccess(Map<String, Object> stringObjectMap) {
 
-                }
+                    }
+
+                    @Override
+                    public void onError(FirebaseError firebaseError) {
+
+                        System.out.println("Email taken error coe: " + FirebaseError.EMAIL_TAKEN);
+                        System.out.println("error: " + FirebaseError.INVALID_EMAIL);
+                        System.out.println("error: " + firebaseError.getCode());
+
+                        //hack line to check if the user email is alread taken assume they are registered
+                        if (firebaseError.getCode() == FirebaseError.EMAIL_TAKEN) {
+                            //already registered case
+                            passInput.setVisibility(View.VISIBLE);
+                            passInput.setHint(R.string.passwordHint);
+                            loginButton.setVisibility(View.VISIBLE);
+                            nextButton.setVisibility(View.GONE);
+                        }
+                        else {
+                            //have user register
+                            Toast.makeText(getApplicationContext(), R.string.registerToast, Toast.LENGTH_LONG).show();
+                            passInput.setVisibility(View.VISIBLE);
+                            passInput.setHint(R.string.setPasswordHint);
+                            registerButton.setVisibility(View.VISIBLE);
+                            nextButton.setVisibility(View.GONE);
+                        }
+                    }
+                });
             }
         });
 
+        //register new user
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Toast.makeText(getApplicationContext(), R.string.registerThanks, Toast.LENGTH_SHORT).show();
-
                 //register user
-                users.put(usernameStr, passInput.getText().toString());
+                //users.put(usernameStr, passInput.getText().toString());
 
-                //Create Bundle to pass username to ControlActivity
-                loginBundle = new Bundle();
-                //assign the values (key, value pairs)
-                loginBundle.putString(context.getString(R.string.username), usernameStr);
+                //create a new firebase user
+                myFirebaseRef.createUser(userInput.getText().toString(), passInput.getText().toString() , new Firebase.ValueResultHandler<Map<String,Object>>() {
 
-                //create intent for control activity
-                Intent intentControl = new Intent(LoginActivity.this, ControlActivity.class);
+                    @Override
+                    public void onSuccess(Map<String, Object> result) {
+                        System.out.println("Successfully created user account with uid: " + result.get("uid"));
+                        //authenticate user
+                        myFirebaseRef.authWithPassword(userInput.getText().toString(), passInput.getText().toString(), new Firebase.AuthResultHandler() {
+                            @Override
+                            public void onAuthenticated(AuthData authData) {
+                                //authenticated successfully
+                                Map<String, String> map = new HashMap<String, String>();
+                                map.put("provider", authData.getProvider());
+                                map.put("more", "more");
+                                myFirebaseRef.child("users").child(authData.getUid()).setValue(map);
+                            }
 
-                //assign the bundle to the intent
-                intentControl.putExtras(loginBundle);
+                            @Override
+                            public void onAuthenticationError(FirebaseError firebaseError) {
+                                //something went wrong
+                                Log.i("onAuthenticatedError", "Error authenticating");
+                            }
+                        });
 
-                //launch control activity
-                LoginActivity.this.startActivity(intentControl);
+                        Toast.makeText(getApplicationContext(), R.string.registerThanks, Toast.LENGTH_SHORT).show();
+
+                        //Create Bundle to pass username to ControlActivity
+                        loginBundle = new Bundle();
+                        //assign the values (key, value pairs)
+                        loginBundle.putString(context.getString(R.string.username), userInput.getText().toString());
+
+                        //create intent for control activity
+                        Intent intentControl = new Intent(LoginActivity.this, ControlActivity.class);
+
+                        //assign the bundle to the intent
+                        intentControl.putExtras(loginBundle);
+
+                        //launch control activity
+                        LoginActivity.this.startActivity(intentControl);
+                    }
+
+                    @Override
+                    public void onError(FirebaseError firebaseError) {
+                        // there was an error
+                        Log.i("onError", "Error creating firebase user");
+                        Toast.makeText(getApplicationContext(), R.string.registerError, Toast.LENGTH_LONG).show();
+                        passInput.setText("");
+                    }
+                });
             }
         });
 
-        //set login button callback
+        //set login button callback for previously registered users
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 //conditional to test if user and password are correct
                 usernameStr = userInput.getText().toString();
-                if (users.containsKey(usernameStr) &&
-                        passInput.getText().toString().equals(users.get(usernameStr)))
-                        {
 
-                            //Create Bundle to pass username to ControlActivity
-                            loginBundle = new Bundle();
-                            //assign the values (key, value pairs)
-                            loginBundle.putString(context.getString(R.string.username), usernameStr);
+                //authenticate user
+                myFirebaseRef.authWithPassword(usernameStr, passInput.getText().toString(), new Firebase.AuthResultHandler() {
+                    @Override
+                    public void onAuthenticated(AuthData authData) {
+                        //authenticated successfully
+                        Map<String, String> map = new HashMap<String, String>();
+                        map.put("provider", authData.getProvider());
+                        map.put("more", "more");
+                        myFirebaseRef.child("users").child(authData.getUid()).setValue(map);
 
-                            //create intent for control activity
-                            Intent intentControl = new Intent(LoginActivity.this, ControlActivity.class);
+                        //Create Bundle to pass username to ControlActivity
+                        loginBundle = new Bundle();
+                        //assign the values (key, value pairs)
+                        loginBundle.putString(context.getString(R.string.username), usernameStr);
 
-                            //assign the bundle to the intent
-                            intentControl.putExtras(loginBundle);
+                        //create intent for control activity
+                        Intent intentControl = new Intent(LoginActivity.this, ControlActivity.class);
 
-                            //launch control activity
-                            LoginActivity.this.startActivity(intentControl);
-                }
-                else {
-                    //display toast for incorrect login attempt
-                    Toast.makeText(getApplicationContext(), R.string.incorrectLogin, Toast.LENGTH_SHORT).show();
-                }
+                        //assign the bundle to the intent
+                        intentControl.putExtras(loginBundle);
+
+                        //launch control activity
+                        LoginActivity.this.startActivity(intentControl);
+                    }
+
+                    @Override
+                    public void onAuthenticationError(FirebaseError firebaseError) {
+                        System.out.println("error: " + firebaseError.INVALID_PASSWORD);
+                        System.out.println("error: " + firebaseError.getCode());
+
+                        //something went wrong
+                        Log.i("onAuthenticatedError", "Error authenticating");
+                        passInput.setText("");
+                        //display toast for incorrect login attempt
+                        Toast.makeText(getApplicationContext(), R.string.incorrectLogin, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
             }
         });
     }
